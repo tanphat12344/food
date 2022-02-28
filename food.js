df669d60ff05d728e85e57d0265e842a76636530
tanphat12344 <letrantanphat160998@gmail.com>
2022-02-28 16:59:58 +0700

    menu click

---
+++ food.js
@@ -0,0 +1,30 @@
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".icon-menu");
+const mainnavbar = document.querySelector(".mainnavbar");
+const closeBtn = document.querySelectorAll(".navbar__link");
+const overlay = document.querySelector("#overlay");
+
+
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("navbar__open");
+    overlay.classList.toggle("show");
+  });
+
+
+//   closeBtn.addEventListener("click", () => {
+//     menu.classList.remove("navbar__open");
+//   });
+
+closeBtn.forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("navbar__open");
+        overlay.classList.remove("show");
+      });
+});
+
+overlay.addEventListener("click", () =>{
+    overlay.classList.remove("show");
+    menu.classList.remove("navbar__open");
+
+});
